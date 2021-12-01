ac2fc246ed10abceef5acf741635355b6aef2723
mbuscemi <matthew.buscemi@gmail.com>
2021-12-01 02:50:16 -0800

    parse the string vector into an integer vector

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -5,9 +5,10 @@ const READ_FAILURE_MSG: &str = "Unable to read file";
 
 fn main() {
     let data = fs::read_to_string(INPUT_FILE_PATH).expect(READ_FAILURE_MSG);
-    let lines: Vec<&str> = data.split('\n').collect();
+    let lines: Vec<&str> = data.split('\n').filter(|line| !line.is_empty()).collect();
+    let depths: Vec<i32> = lines.iter().map(|line| line.parse::<i32>().unwrap()).collect();
 
-    for line in lines {
-        println!("{}", line);
+    for depth in depths {
+        println!("{}", depth);
     }
 }
