5ca45279b6f6116000cac17c546f9313e45ef7da
mbuscemi <matthew.buscemi@gmail.com>
2021-12-01 02:35:36 -0800

    made the code read the file as a string

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,3 +1,9 @@
+use std::fs;
+
+const INPUT_FILE_PATH: &str = "data/input.txt";
+const READ_FAILURE_MSG: &str = "Unable to read file";
+
 fn main() {
-    println!("Hello, world!");
+    let data = fs::read_to_string(INPUT_FILE_PATH).expect(READ_FAILURE_MSG);
+    println!("Input data: {}", data);
 }
