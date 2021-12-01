e97aaab294a96d51ce925e62da27a1cd5a148341
mbuscemi <matthew.buscemi@gmail.com>
2021-12-01 02:39:51 -0800

    split each line of the file into a vector of strings; printed each line

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -5,5 +5,9 @@ const READ_FAILURE_MSG: &str = "Unable to read file";
 
 fn main() {
     let data = fs::read_to_string(INPUT_FILE_PATH).expect(READ_FAILURE_MSG);
-    println!("Input data: {}", data);
+    let lines: Vec<&str> = data.split('\n').collect();
+
+    for line in lines {
+        println!("{}", line);
+    }
 }
