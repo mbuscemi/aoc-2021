df7602fdbd471a65a21fcf4fc41a90875865839b
mbuscemi <matthew.buscemi@gmail.com>
2021-12-01 03:21:03 -0800

    completed part two

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -7,8 +7,10 @@ fn main() {
     let data = fs::read_to_string(INPUT_FILE_PATH).expect(READ_FAILURE_MSG);
     let lines: Vec<&str> = data.split('\n').filter(|line| !line.is_empty()).collect();
     let depths: Vec<i32> = lines.iter().map(|line| line.parse::<i32>().unwrap()).collect();
+    let triples = triples(&depths);
 
-    println!("number of increases: {}", num_increases(depths));
+    println!("number of single-value depth increases: {}", num_increases(depths));
+    println!("number of triple-value depth increases: {}", num_increases(triples));
 }
 
 fn num_increases(depths: Vec<i32>) -> i32 {
@@ -24,4 +26,17 @@ fn num_increases(depths: Vec<i32>) -> i32 {
     }
 
     increases
+}
+
+fn triples(depths: &[i32]) -> Vec<i32> {
+    let mut triples = Vec::new();
+
+    for index in 0..depths.len()-2 {
+        let first = depths[index];
+        let second = depths[index + 1];
+        let third = depths[index + 2];
+        triples.push(first + second + third);
+    }
+
+    triples
 }
